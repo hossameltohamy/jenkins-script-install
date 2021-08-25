abf51cbd239506321c273f20e977865d41e02fc9
hossam yahia <hossam@dafholding.com>
2021-08-25 18:27:16 +0200

    fix

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -17,7 +17,8 @@ usermod -aG docker ubuntu
 # run jenkins
 mkdir -p /var/jenkins_home
 chown -R 1000:1000 /var/jenkins_home/
-docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts-jdk11
+
+docker run -p 8080:8080 -p 50000:50000  -v /var/run/docker.sock:/var/run/docker.sock -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts-jdk11
 
 # show endpoint
 echo 'Jenkins installed'
